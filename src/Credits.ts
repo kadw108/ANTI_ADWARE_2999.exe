347262dfa7ae7071eea3e383562bb956f9e4fdae
kadw108 <wallwritinglast@gmail.com>
2024-05-20 09:14:42 -0400

    Last minute changes

--- src/Credits.ts
+++ src/Credits.ts
@@ -6,7 +6,7 @@ export default class Credits extends Phaser.Scene {
     }
 
     create(): void {
-        const lines = this.add.text(CONSTANTS.originX, 10, "The combat song is WEARY WILLOW by EM ESSEX.\nIt is the property of HALLEY LABS and does not\nbelong to me (KADW).\n\nMost of the other art assets were created by me\n(including the main menu music)\nand all of the other art assets are \nin the public domain.\nDo whatever you want with them.\n\nAll text uses the VT323 font by Peter Hull,\nwhich is under the Open Font License.", { fontFamily: "DisplayFont", fontSize: 30, color: "#ffffff" });
+        const lines = this.add.text(CONSTANTS.originX, 10, "The combat song is WEARY WILLOW by EM ESSEX.\nIt is the property of HALLEY LABS and does not\nbelong to me (KADW).\nSee heckscaper.com/res.php for more info.\n\nMost of the other art assets were created by me\n(including the main menu music)\nand all of the other art assets are \nin the public domain.\nDo whatever you want with them.\n\nAll text uses the VT323 font by Peter Hull,\nwhich is under the Open Font License.\n\nI made this small game in 5 days\nfor Bullet Hell Jam 5.\nI hope it is fun.", { fontFamily: "DisplayFont", fontSize: 30, color: "#ffffff" });
         lines.setOrigin(0.5, 0);
 
         const subtitle = this.add.text(CONSTANTS.originX, CONSTANTS.originY + 265, "CLICK ANYWHERE TO RETURN", { fontFamily: "DisplayFont", fontSize: 40, color: "#ffffff" });
--- src/Menu.ts
+++ src/Menu.ts
@@ -2,15 +2,19 @@ import { CONSTANTS } from "./CONSTANTS_FILE";
 
 export default class Menu extends Phaser.Scene {
 
-    posthuman: Phaser.Sound.NoAudioSound | Phaser.Sound.WebAudioSound | Phaser.Sound.HTML5AudioSound;
+    posthuman: Phaser.Sound.NoAudioSound | Phaser.Sound.WebAudioSound | Phaser.Sound.HTML5AudioSound | null;
 
     constructor() {
         super("Menu");
+        this.posthuman = null;
     }
 
     create(): void {
-        this.posthuman = this.sound.add("posthuman", { loop: true, delay: 0 });
-        this.posthuman.play();
+        if (this.posthuman === null) {
+            this.posthuman = this.sound.add("posthuman", { loop: true, delay: 0 });
+            this.posthuman.play();
+            this.sound.pauseOnBlur = false;
+        }
 
         const title = this.add.text(CONSTANTS.originX, CONSTANTS.originY - 30, "ANTI_ADWARE_2999.EXE", { fontFamily: "DisplayFont", fontSize: 70, color: "#ffffff" });
         title.setOrigin(0.5, 0.5);
@@ -29,7 +33,10 @@ export default class Menu extends Phaser.Scene {
         subtitle.setOrigin(0.5, 0.5);
         subtitle.setInteractive();
         subtitle.on("pointerdown", () => {
-            this.posthuman.destroy();
+            if (this.posthuman !== null) {
+                this.posthuman.destroy();
+                this.posthuman = null;
+            }
             this.scene.start("GameMain");
         });
 
